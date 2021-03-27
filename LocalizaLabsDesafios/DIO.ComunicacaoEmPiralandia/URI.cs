2b1d8118a577a347b4b778c1dedaad24a48e4a10
memandacaru <maria.elisa@otempo.com.br>
2021-03-27 19:00:43 -0300

    cadastro de series

---
+++ LocalizaLabsDesafios/DIO.ComunicacaoEmPiralandia/URI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DIO.ComunicacaoEmPiralandia
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            char[] arr = n.ToCharArray();
+
+            Array.Reverse(arr);
+            string v = new string(arr);
+
+            Console.WriteLine(v);
+        }
+    }
+}
